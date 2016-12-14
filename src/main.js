b9d544bbd593e1c44f67a96629e1837e2d7d7899
Punto_Lin <punto_lin@wistron.com>
2016-12-14 13:36:32 +0800

    #4 React homework

--- src/main.js
+++ src/main.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Hello extends React.Component {
-  render(){
-    return (<div>Hel123ljoijoijojo</div>);
-  }
+class Panel extends React.Component {
+	constructor() {
+		super();
+		this.state = { 
+			number:0 
+		};
+	}
+	btnClick = () => {
+		let newState = {
+			number: this.state.number + 1
+		};
+		this.setState(newState);
+	};
+	render() {
+		return (
+			<div>
+				<Show number={this.state.number} />
+				<Btn handleClick={this.btnClick} />
+			</div>
+		);
+	}
 }
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+class Btn extends React.Component {
+	render() {
+		return (
+			<button onClick={this.props.handleClick}> Click Me </button>
+		);
+	}
+}
+
+class Show extends React.Component {
+	render() {
+		return (
+			<div>{this.props.number}</div>
+		);
+	}
+}
+
+
+
+ReactDOM.render(<Panel />, document.getElementById('app'));
